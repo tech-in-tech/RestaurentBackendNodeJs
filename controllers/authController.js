fb7af4f10fcf384e03aba32d2cb27e753e771ff0
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-08-10 20:27:35 +0530

    Create login route | hashed the password using bcrypt | add authentication using JWT auth | add token functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModel");
-
+const bcrypt = require('bcryptjs')
+const JWT = require('jsonwebtoken')
 // REGISTER
 const registerController = async (req, res) => {
   try {
@@ -19,8 +20,11 @@ const registerController = async (req, res) => {
         message: "Email Already REgistered please Login"
       })
     }
+    // Hashing password
+    let salt = bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hash(password,salt)
     // creating new User
-    const user = await userModel.create({ userName, email, password, phone, address })
+    const user = await userModel.create({ userName, email, password:hashedPassword, phone, address })
     res.status(201).send({
       success: true,
       message: "Successfully Register",
@@ -35,4 +39,51 @@ const registerController = async (req, res) => {
     })
   }
 }
-module.exports = { registerController };
+
+// LOGIN controller
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Validation
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: 'Please Provide Email or Password'
+      })
+    }
+    // Cheak User
+    const user = await userModel.findOne({ email})
+    // Validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'User Not Found'
+      })
+    }
+    // Check user password || compare password 
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+      return res.status(500).send({
+        success: false,
+        message:"Invalid Password",
+      });
+    }
+    const token = JWT.sign({id:user._id},process.env.JWT_SECRET,{
+      expiresIn:"7D"
+    })
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      token,
+      user,
+    })
+  } catch (error) {
+    console.log("LoginController :: ", error);
+    res.status(500).send({
+      success: false,
+      message: 'Error in Login API',
+      error
+    })
+  }
+}
+module.exports = { registerController, loginController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const  express = require('express');
-const { registerController } = require('../controllers/authController');
+const { registerController, loginController } = require('../controllers/authController');
 const router = express.Router()
 
 // routes
 // REGISTER || POST
 router.post('/register',registerController)
 
+// LOGIN || POST
+router.post('/login',loginController)
 module.exports = router
